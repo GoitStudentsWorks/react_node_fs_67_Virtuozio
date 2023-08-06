b85b6669a0671e2bead2c24c5ebb9bc701b9eda3
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-06 21:58:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/YourPet">
+        <BrowserRouter basename="/react_node_fs_67_Virtuozio">
           <App />
         </BrowserRouter>
       </PersistGate>
